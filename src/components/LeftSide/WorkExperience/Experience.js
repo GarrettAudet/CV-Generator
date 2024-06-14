ceee651044200cbfbcef91ff436f4dc4fa6bb06c
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-14 00:36:59 -0400

    Added state details

--- src/components/LeftSide/WorkExperience/Experience.js
+++ src/components/LeftSide/WorkExperience/Experience.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { faToolbox } from '@fortawesome/free-solid-svg-icons';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import './Experience.css';  
 import '../CommonStyles/styles.css'
 
 export default function Experience() {
+    // Initialize state for each detail with default values
+    const [details, setDetails] = useState({
+        jobTitle: 'Senior Analyst',
+        company: 'Accenture',
+        startDate: 'July 2021',
+        endDate: 'November 2023',
+        description: ''
+    });
+
+    // Function to handle change in input fields
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setDetails(prevDetails => ({
+            ...prevDetails,
+            [name]: value
+        }));
+    };
+
+    // HTML 
     return (
         <div className="Experience LHSInput">
             <h2 className="LHSInputHeader">
