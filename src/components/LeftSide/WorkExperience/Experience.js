63e3df98e96887089bbdd766024e02e0eb49216e
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-18 17:08:29 -0400

    Added properties to properties to experience array

--- src/components/LeftSide/WorkExperience/Experience.js
+++ src/components/LeftSide/WorkExperience/Experience.js
@@ -10,25 +10,31 @@ export default function Experience() {
     const [isVisible, setIsVisible] = useState(false);
     const [jobs, setJobs] = useState([
         {
+            id: 1,
             jobTitle: 'Senior Analyst',
             company: 'Accenture',
             startDate: new Date(2021, 6),
             endDate: new Date(2023, 10),
-            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America",
+            isVisible: false
         },
         {
+            id: 2,
             jobTitle: 'Co-Founder',
             company: 'MarText',
             startDate: new Date(2019, 6),
             endDate: new Date(2021, 6),
-            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America",
+            isVisible: false
         },
         {
+            id: 3,
             jobTitle: 'Intelligence Operator',
             company: 'Canadian Armed Forces',
             startDate: new Date(2017, 6),
             endDate: new Date(2019, 5),
-            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America",
+            isVisible: false
         }
     ]);
 
