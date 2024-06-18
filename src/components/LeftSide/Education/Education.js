315e11d310401ae709b044942b7738854bfa36cd
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-18 16:53:00 -0400

    Added education and visible states

--- src/components/LeftSide/Education/Education.js
+++ src/components/LeftSide/Education/Education.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSchool } from '@fortawesome/free-solid-svg-icons';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +6,17 @@ import './Education.css';
 import '../CommonStyles/styles.css'
 
 export default function Experience() {
+    // Set Visible State
+    const [isVisible, setIsVisible] = useState(false);
+
+    // Education 
+    const [education, setEducation] = useState([{
+        school: "Queen's Universty",
+        degree: 'Bachelor of Commerce',
+        startDate: new Date(2021, 6),
+        endDate: new Date(2023, 10),
+    }]);
+
     return (
         <div className="Experience LHSInput">
             <h2 className="LHSInputHeader">
