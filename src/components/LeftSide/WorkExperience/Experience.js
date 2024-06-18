d139b8d22454fd45efd59d52a57697673f657af9
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-18 16:59:23 -0400

    Added objects to initialization of experience

--- src/components/LeftSide/WorkExperience/Experience.js
+++ src/components/LeftSide/WorkExperience/Experience.js
@@ -8,13 +8,29 @@ import '../CommonStyles/styles.css';
 
 export default function Experience() {
     const [isVisible, setIsVisible] = useState(false);
-    const [jobs, setJobs] = useState([{
-        jobTitle: 'Senior Analyst',
-        company: 'Accenture',
-        startDate: new Date(2021, 6),
-        endDate: new Date(2023, 10),
-        description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
-    }]);
+    const [jobs, setJobs] = useState([
+        {
+            jobTitle: 'Senior Analyst',
+            company: 'Accenture',
+            startDate: new Date(2021, 6),
+            endDate: new Date(2023, 10),
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+        },
+        {
+            jobTitle: 'Co-Founder',
+            company: 'MarText',
+            startDate: new Date(2019, 6),
+            endDate: new Date(2021, 6),
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+        },
+        {
+            jobTitle: 'Intelligence Operator',
+            company: 'Canadian Armed Forces',
+            startDate: new Date(2017, 6),
+            endDate: new Date(2019, 5),
+            description: "Developed a comprehensive go-to-market, growth, and business development strategy for Accenture’s Strategy and Machine Learning Practice, securing key sales inroads across 3 industries, and recognition as a top revenue opportunity in North America"
+        }
+    ]);
 
     const handleChange = (index, field, value) => {
         setJobs(prevJobs => prevJobs.map((job, idx) => idx === index ? {...job, [field]: value} : job));
