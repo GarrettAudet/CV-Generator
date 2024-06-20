593d91337a756cb9c6530c03b56ed8688699ee49
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-20 19:09:15 -0400

    Updated volunteer section

--- src/components/LeftSide/DataList/Sections/Volunteering.js
+++ src/components/LeftSide/DataList/Sections/Volunteering.js
@@ -4,7 +4,7 @@ import { volunteerData } from '../SampleData/SampleData';
 import DataList from '../DataList';
 
 function Volunteering() {
-    return <DataList data={volunteerData} delineate = {","} setData={() => {}} icon={faHandshakeAngle} sectionTitle="Experience" addText="Add Experience" />;
+    return <DataList data={volunteerData} delineate = {","} setData={() => {}} icon={faHandshakeAngle} sectionTitle="Volunteering" addText="Add Volunteering" />;
 }
 
 export default Volunteering;
