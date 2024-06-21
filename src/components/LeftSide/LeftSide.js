814f6a532a3c760aed64b44e3ede0c8a997ff9f6
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-21 19:01:41 -0400

    Updated LHS to control state with clear and load

--- src/components/LeftSide/LeftSide.js
+++ src/components/LeftSide/LeftSide.js
@@ -1,27 +1,52 @@
-/* Import Library and Relevant Information */ 
-import React from 'react';
+import React, { useState } from 'react';
 import SelectOption from './Option/selectOption';
-
-/* Import Components */
-import ResumeManager from './Option/ResumeManager'
 import PersonalDetails from './PersonalDetails/PersonalDetails'
 import Experience from './DataList/Sections/Experience'
 import Education from './DataList/Sections/Education'
 import Volunteering from './DataList/Sections/Volunteering'
 import Awards from './DataList/Sections/Awards'
+import './LeftSide.css';
 
-/* Import Styling for Left Side */
-import './LeftSide.css';  
+// Assuming you have import paths and initial data available
+import {
+  initialJobsData,
+  initialEducationData,
+  initialVolunteerData,
+  initialAwardsData,
+  emptyjobsData,
+  emptyeducationData,
+  emptyvolunteerData,
+  emptyawardsData
+} from './DataList/SampleData/SampleData';  
 
 export default function LeftSide() {
+    const [jobs, setJobs] = useState(initialJobsData);
+    const [education, setEducation] = useState(initialEducationData);
+    const [volunteer, setVolunteer] = useState(initialVolunteerData);
+    const [awards, setAwards] = useState(initialAwardsData);
+
+    const clearData = () => {
+        setJobs(emptyjobsData);
+        setEducation(emptyeducationData);
+        setVolunteer(emptyvolunteerData);
+        setAwards(emptyawardsData);
+    };
+
+    const loadData = () => {
+        setJobs(initialJobsData);
+        setEducation(initialEducationData);
+        setVolunteer(initialVolunteerData);
+        setAwards(initialAwardsData);
+    };
+
     return (
         <div className="leftSide">
-            <ResumeManager />
+            <SelectOption clearData={clearData} loadData={loadData} />
             <PersonalDetails />
-            <Experience />
-            <Education />
-            <Volunteering />
-            <Awards />
+            <Experience jobsData={jobs} />
+            <Education educationData={education} />
+            <Volunteering volunteerData={volunteer} />
+            <Awards awardsData={awards} />
         </div>
     );
 }
