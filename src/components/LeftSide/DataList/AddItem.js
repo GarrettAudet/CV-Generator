adddb5512bb1cb370f7b510731a06f050024b48c
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-21 22:12:39 -0400

    Edited AddItem to add onClick function

--- src/components/LeftSide/DataList/AddItem.js
+++ src/components/LeftSide/DataList/AddItem.js
@@ -4,10 +4,10 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './AddItem.css';
 
 /* This is the Function that Enables an Item to be Added to an Array (Experience, Education, etc.) */
-export default function AddItem({ headerText, isVisible, toggle }) {
+export default function AddItem({ headerText, isVisible, insertNew }) {
     return (
         <div className={`addExperienceContainer ${isVisible ? 'visible' : 'hidden'}`}>
-            <div className='addExperience' onClick={toggle}>
+            <div className='addExperience' onClick={insertNew}>
                 <FontAwesomeIcon icon={faPlus} className = "faPlus"/>
                 <h4>{headerText}</h4>
             </div>
